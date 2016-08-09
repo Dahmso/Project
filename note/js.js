52d7aa92954f54023ac36a17110976893d83739e
Dahmani <dahmani.sof@gmail.com>
2016-08-09 15:28:57 +0200

    exo fini

--- note/js.js
+++ note/js.js
@@ -1,6 +1,16 @@
 // Amene à une autre page.
-window.location = "";
-document.location = "";
+  window.location = "";
+  document.location = "";
 // Recharge la page.
-window.location.reload();*/
-//
+  window.location.reload();*/
+// Retour à la ligne dans une chaîne de caractère.
+  "Ceci est une chaîne \nSur plusieurs lignes."
+// Dernier élément d'un table ou d'une chaîne de caractère.
+  var tableau = [1,2,3,4]; tableau.length - 1;
+  var chaine = "chaine"; chaine.length - 1;
+// Acceder caractere via l'indice.
+  tableau.charAt(0) == tableau[0];
+// Décrementer dans une condition.
+  for (var i = userResponse.length - 1; i >= 0; i--)
+// Crée un espace en JavaScript / les \ sont la pour intégrer les doubles guillemets dans un string
+"Quoth the raven:\t\"Nevermore!\"";
